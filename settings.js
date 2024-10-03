f3b9357857e082ba81c5bbd708e7b425290e4445
raffipedia <dragsstore1@gmail.com>
2024-10-04 00:46:46 +0700

    master

---
+++ settings.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+
+global.creator = 'Ryuu'// yourname
+global.MONGO_DB_URI = "mongodb+srv://dragskeyrgr:FImOLBxVqf8nGwor@cluster0.s8ggl.mongodb.net/dragskeyrgr?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.your_email = "dragsstore1@gmail.com" //email
+global.email_password = "opsb pegq hxwy ktvh" //application password email
+global.limitCount = 10000
+global.YUOR_PORT = 8000
+global.loghandler = {
+	noapikey:{
+		status: 403,
+        message: 'Input parameter apikey',
+        creator: `${creator}`,
+        result: "error"
+    },
+    error: {
+        status: 503,
+        message: 'Service Unavaible, Sedang dalam perbaikan',
+        creator: `${creator}`
+    },
+    apikey: {
+    	status: 403,
+    	message: 'Forbiden, Invalid apikey',
+    	creator: `${creator}`
+    },
+    noturl: {
+    	status: 403,
+    	message: 'Forbiden, Invlid url, masukkan parameter url',
+    	creator: `${creator}`,
+    }
+}
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+	delete require.cache[file]
+	require(file)
+})
